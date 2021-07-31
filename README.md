0090fe4a8b164b0d2d01e8478b9913ae4a3365c3
Hyacinth-zgy <838673371@qq.com>
2021-07-31 18:14:07 +0800

    打包css 文件

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ webpack会以./src/index.js 为入口开始打包，打包后输出到./build.js
 ######## webpack 默认可以打包JS,JSON 文件 不能打包css,less,vue...等其他文件，其他文件文件要用loader翻译
 
 ######## 打包css文案件
+
+######## 使用webpack.config.js配置文件,该文件指示wenpck怎么工作，所有构件工具都是基于node环境运行的，模块化默认使用common.js
+
+######## 打包CSS文件loader  npm i style-loader -D  npm i css-loader -D
+         style-loader:创建style标签，将js中的样式资源进行插入到head中生效
+         css-loader:将css文件以common.js模块规范加载到js文件中，里面的内容是样式字符串
+         配置:
+    {
+        test:/\.css$/,
+        use:[
+          'style-loader','css-loader'
+        ]
+    }
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// webpack的配置文件
+// 运行webpakck是会加载此文件
+
+const { resolve }  = require('path');
+const config = {
+  resolve: {
+    // 在导入语句没带文件后缀时，Webpack 会自动带上后缀后去尝试访问文件是否存在。  resolve.extensions 用于配置在尝试过程中用到的后缀列表，默认是：
+    extensions: ['.js', '.ts', '.json'],
+  },
+  // mode: 'production', 这里不指定mode，通过脚本命令传递参数进来
+  entry: './src/index.js',
+  output: {
+    filename:'build.js',
+    path:resolve(__dirname,'build')
+  },
+  module: {
+    rules: [
+      {
+        test:/\.css$/,
+        use:[
+          'style-loader','css-loader'
+        ]
+      }
+    ],
+  },
+  plugins: [],
+};
+
+module.exports = (env, argv) => {
+    if(argv.mode==='development'){
+    config.output.path = resolve(__dirname,'devbuild')
+    }
+
+  return config;
+};
